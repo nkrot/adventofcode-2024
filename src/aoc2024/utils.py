3ce7a76eaddc3f1aa75bb299aa412194bcd3745c
Nikolai Krot <talpus@gmail.com>
2024-12-13 19:01:00 +0100

    refactor aoc2024.utils
    * extend `to_numbers()` to tokenize by any non-digit character
    * add `group_lines()` to regroup lines around empty lines

--- src/aoc2024/utils.py
+++ src/aoc2024/utils.py
@@ -3,6 +3,8 @@ General purpose utilities for AOC2024
 """
 
 import os
+import re
+import functools
 from typing import Any, Callable, Optional
 
 
@@ -18,8 +20,9 @@ def from_env(var: str = "DEBUG", alt = None) -> Optional[str|int]:
 
 
 def load_input(
-    fpath: str = None,
+    fpath: str,
     *,
+    by_blocks: bool = False,
     line_parser: Callable = None,
 ) -> list[Any]:
     """
@@ -27,13 +30,24 @@ def load_input(
 
     Arguments
     ---------
+    fpath : str, optional, default = "input.txt"
+        path to the file with input data
 
     Keyword-only arguments
     ----------------------
+    by_blocks : bool, default = False
+        treat empty lines in the file as separators between groups of lines
+        and make lines into groups. This adds an outer list
+    line_parser: Callable
+        a function that will be envoked on every line. Useful for converting
+        the line to something else, e.g. to a list of numbers
 
     Returns
     -------
-
+    list[list[str]] :
+        if by_blocks is True
+    list[str] :
+        if by_blocks is False
     """
 
     fpath = fpath or "input.txt"
@@ -41,15 +55,48 @@ def load_input(
     with open(fpath) as ifd:
         lines = list(map(str.strip, ifd.readlines()))
 
+    if by_blocks:
+        lines = group_lines(lines)
+
     if line_parser:
-        lines = list(map(line_parser, lines))
+        if by_blocks:
+            lines = [
+                list(map(line_parser, lns))
+                for lns in lines
+            ]
+        else:
+            lines = list(map(line_parser, lines))
 
     return lines
 
 
+def group_lines(lines: list[str]) -> list[list[str]]:
+    """
+    Make lines into groups of lines.
+    An empty line is treated as a separator between groups.
+
+    Arguments
+    ---------
+    lines: list of strings
+
+    Returns
+    -------
+    list[list[str]] : a list of groups, where each group is a list of lines
+    """
+    def _group(lst, line):
+        if len(line) == 0:
+            lst.append([])
+        else:
+            lst[-1].append(line)
+        return lst
+
+    return functools.reduce(_group, lines, [[]])
+
+
 def to_numbers(line: str, cls=int) -> list[int]:
     """
-    Split line by whitespace (one or many) to a list of integers.
+    Extract all numbers from given string `line`.
+    The string is tokenized by any non-numerical character.
 
     Arguments
     ---------
@@ -61,6 +108,11 @@ def to_numbers(line: str, cls=int) -> list[int]:
     Returns
     -------
     A list of objects of type `cls`
+
+    Note
+    ----
+    Does not work for recognizing float numbers (that are point or comma
+    separated).
     """
 
-    return list(map(cls, line.split()))
+    return list(map(int, re.findall(r'\d+', line)))
