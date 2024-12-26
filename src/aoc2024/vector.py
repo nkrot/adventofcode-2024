7132e6acbf7ee17e026fa824dbaa315f8746363d
Nikolai Krot <talpus@gmail.com>
2024-12-26 19:39:07 +0100

    add new method vector.mul() (multiply)

--- src/aoc2024/vector.py
+++ src/aoc2024/vector.py
@@ -1,4 +1,6 @@
 
+# original_abs = abs
+
 def sub(u: list[int], v: list[int]) -> list[int]:
     """Subtract two lists elementwise"""
     len_u, len_v = len(u), len(v)
@@ -25,3 +27,21 @@ def add(u: list[int], v: list[int]) -> list[int]:
         for i in range(len_u)
     ]
     return type(u)(res)
+
+
+def mul(u: list[int], v: list[int]) -> list[int]:
+    """Multiply two lists elementwise"""
+    len_u, len_v = len(u), len(v)
+    if len_u != len_v:
+        raise ValueError(
+            f"Vectors must of the the same length but got {len_u} and {len_v}"
+        )
+    res = [
+        u[i] * v[i]
+        for i in range(len_u)
+    ]
+    return type(u)(res)
+
+
+# def abs(v: list[int]) -> list[int]:
+#     return type(v)(original_abs(val) for val in v)
